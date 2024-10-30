74dd1a0be89aaec0119dce2cb2c3dbb2686d2f09
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-30 10:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentActive = 1;
+
+nextButton.addEventListener('click', () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  updateProgress();
+});
+
+prevButton.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  updateProgress();
+});
+
+function updateProgress() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  prevButton.disabled = currentActive === 1;
+  nextButton.disabled = currentActive === circles.length;
+}
